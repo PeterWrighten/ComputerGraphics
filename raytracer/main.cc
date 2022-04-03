d2aca1f04a4a504c5fb03a9de9f18b569e289022
PeterWrght <peterwrighten@gmail.com>
2022-04-04 03:01:43 +0900

    update::raytracer::fade

--- raytracer/main.cc
+++ raytracer/main.cc
@@ -5,16 +5,40 @@
 #include<iostream>
 using namespace std;
 
+color ray_color(const ray &r) { //simple camera
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return color(1.0, 1.0, 1.0) * (1 - t) + color(0.5, 0.7, 1.0) * t;
+}
+
 int main() {
-    const int height = 256;
-    const int width = 256;
+
+    //Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int width = 400;
+    const int height = static_cast<int>(width / aspect_ratio);
+
+    //Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+
 
     cout<< "P3\n" << width << ' ' << height << "\n255\n";
 
     for (int j = height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < width; ++i) {
-            color pixel_color(double(i)/(width - 1), double(j)/(height - 1), 0.25);
+            auto u = double(i)/(width - 1);
+            auto v = double(j)/(height - 1);
+            ray r(origin, lower_left_corner + horizontal * u + vertical * v - origin);
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
             // auto r = double(i) / (width-1);
             // auto g = double(j) / (height-1);
