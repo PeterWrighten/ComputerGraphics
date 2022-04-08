148042078618e9f559bb4d93a79f5188eae4fef4
PeterWright <peterwrighten@gmail.com>
2022-04-08 16:57:00 +0900

    update::render

--- raytracer/main.cc
+++ raytracer/main.cc
@@ -11,14 +11,20 @@ bool hit_sphere(const point3& center, double radius, const ray& r) {
     auto b = 2.0 * dot(oc, r.direction());
     auto c = dot(oc, oc) - radius * radius;
     auto discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
+    if(discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - sqrt(discriminant) / (2.0 * a));
+    }
 }
 
 color ray_color(const ray &r) { //simple camera
-    if (hit_sphere(point3(0, 0, -1), 0.5, r))
-        return color(1, 0, 0);
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if(t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return color(N.x() + 1, N.y() + 1, N.z() + 1) * 0.5;
+    }
     vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);
     return color(1.0, 1.0, 1.0) * (1 - t) + color(0.5, 0.7, 1.0) * t;
 }
 
