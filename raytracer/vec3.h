844e75291c58f332b62a2e471a706e1f785a2f35
PeterWrighten <peterwrighten@gmail.com>
2022-09-03 18:46:57 +0900

    update: sphere

--- raytracer/vec3.h
+++ raytracer/vec3.h
@@ -13,7 +13,7 @@ class vec3 {
     public:
         double e[3];
     public:
-        vec3() : e{0, 0, 0} {}
+        vec3() : e{0.0, 0.0, 0.0} {}
         vec3(double e0, double e1, double e2): e{e0, e1, e2} {}
 
         double x() const { return e[0]; }
